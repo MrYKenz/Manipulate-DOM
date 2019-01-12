8899882ec3ccd0752fa18b3fcfaab6d2c3498497
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-01-12 19:30:40 +0000

    exercise 2

---
+++ domID.js
@@ -0,0 +1,10 @@
+// GETELEMENTBYID //
+
+var a = document.getElementById('header-title');
+console.log(a.textContent);
+console.log(a.innerText);
+console.log(a.innerHTML);
+a.style.borderBottom = 'solid 2px #FFFFFF'
+
+var b = document.getElementById('main-header');
+b.style.borderBottom = 'solid 2px #000000'
